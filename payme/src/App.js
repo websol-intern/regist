f76d02f828f6999f00b789c2bc99aebd8d97ade3
websol-intern <aman.wesol.intern@gmail.com>
2021-05-18 14:30:44 +0530

    my first commit

---
+++ payme/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import GooglePayButton from '@google-pay/button-react';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <h1><img src={logo} className="App-logo" alt="logo" /></h1>
+      
+      <h1>SHIVANSH</h1>
+      
+    </div>
+  );
+}
+
+export default App;
